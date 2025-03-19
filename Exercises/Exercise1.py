84830b92e0b2c71109072f76c27e6c15ab5bedea
Kristel Geraldine Villalta Porras <158532211+KrisGerald02@users.noreply.github.com>
2025-03-18 18:22:36 -0600

    Update and rename ui.py to Exercise1.py

---
+++ Exercises/Exercise1.py
@@ -0,0 +1,11 @@
+#Comentario de una linea
+"""
+Impresion de los datos personales dle estudiante
+"""
+
+print("Datos del Estudiante")
+print("=" *20)
+print("Nombre: ", end=" ")#end no salto de linea
+print("Jose Alejandro")
+print("Apellidos: ", end=" ")
+print("Duran Garcia")
